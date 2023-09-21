5727aeadfdb237a169698e2a730b1535447a9821
LUIS <luis.enrique.enc@gmail.com>
2023-09-20 21:42:00 -0600

    Initial Commit

---
+++ SeriesYPeliculas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeriesYPeliculas
+//
+//  Created by LUIS GONZALEZ on 20/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
