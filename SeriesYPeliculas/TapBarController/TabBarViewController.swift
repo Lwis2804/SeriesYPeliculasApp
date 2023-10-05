0702179482debdb7867fd82d4da096396e826472
LUIS <luis.enrique.enc@gmail.com>
2023-10-04 21:53:19 -0600

    [add] se eliminan 4 vistas y se agrego la copia de código en tab bar

--- SeriesYPeliculas/TapBarController/TabBarViewController.swift
+++ SeriesYPeliculas/TapBarController/TabBarViewController.swift
@@ -8,9 +8,11 @@ class TabBarViewController: UITabBarController {
         super.viewDidLoad()
         let view1 = self.createNavController(for: PlayingNowViewController(), title: "Playing Now", image: UIImage(systemName: "play.fill") ?? UIImage())
         let view2 = self.createNavController(for: MostPopularViewController(), title: "Most popular", image: UIImage(systemName: "star.fill") ?? UIImage())
-        self.viewControllers = [ view1, view2]
+        let view3 = self.createNavController(for: TopRatedViewController(), title: "Top Rated", image: UIImage(systemName: "square.3.stack.3d.top.fill") ?? UIImage())
+        let view4 = self.createNavController(for: UpcomingViewController(), title: "Upcoming", image: UIImage(systemName: "arrowshape.zigzag.forward.fill") ?? UIImage())
+        let view5 = self.createNavController(for: SearchViewController(), title: "Search", image: UIImage(systemName: "magnifyingglass") ?? UIImage())
+        self.viewControllers = [ view1, view2, view3, view4, view5]
     }
-    
     
     
     
